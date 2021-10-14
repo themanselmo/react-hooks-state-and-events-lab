d0cfc1e823f4a8d076ddc11bec759de7ce9a5497
max anselmo <itmanselmo@gmail.com>
2021-10-14 12:00:20 -0400

    added dark mode functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
@@ -6,13 +6,18 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const [lightOrDark, setMode] = useState(false)
+  const appClass = lightOrDark ? "App dark" : "App light"
+
+  function handleClick() {
+    setMode((lightOrDark) => !lightOrDark)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
